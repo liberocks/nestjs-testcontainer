9b06550c0b1418db6dd960eb3c248e65af94b4ac
tirtadwipa.manunggal <trikarsa.manunggal@mclinica.com>
2022-08-12 11:06:59 +0700

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [MongooseModule.forRoot(process.env['MONGODB_URI']), CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
